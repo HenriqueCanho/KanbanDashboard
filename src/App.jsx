0a64987fdc859b8b7a58b647e7c04748e744bd95
Henrique Canho <hmcanho@gmail.com>
2024-09-30 20:44:51 -0300

    style: add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme/theme';
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme/theme.js
@@ -0,0 +1,21 @@
+const theme = {
+    colors: {
+        background: { rgb: 'rgb(245, 245, 245)', rgba: 'rgba(245, 245, 245, 1)' },
+        black: { rgb: 'rgb(0, 0, 0)', rgba: 'rgba(0, 0, 0, 1)' },
+        green: { rgb: 'rgb(128, 211, 1)', rgba: 'rgba(128, 211, 1, 1)' },
+        lightGreen: { rgb: 'rgb(163, 190, 113)', rgba: 'rgba(163, 190, 113, 1)' },
+        yellow: { rgb: 'rgb(255, 255, 0)', rgba: 'rgba(255, 255, 0, 1)' },
+        olive: { rgb: 'rgb(143, 187, 70)', rgba: 'rgba(143, 187, 70, 1)' }
+    },
+
+    getColor: (colorName, alpha) => {
+        const color = theme.colors[colorName];
+        if (alpha !== undefined) {
+            const [r, g, b] = color.rgb.match(/\d+/g).map(Number);
+            return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+        }
+        return color.rgb;
+    }
+};
+
+export default theme;
